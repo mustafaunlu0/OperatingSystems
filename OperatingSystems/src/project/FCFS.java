5ebea516b9ec156cae98e67da66b52bd6f04daf9
emirhanusta <emirhan1usta@gmail.com>
2022-12-25 19:39:04 +0300

    FCFS file added

---
+++ OperatingSystems/src/project/FCFS.java
@@ -0,0 +1,45 @@
+package project;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FCFS {
+	Queue<Proses> realTimeQueue;
+	Queue<Proses> readyQueuePrio0=new LinkedBlockingQueue<Proses>();
+
+	public FCFS(Queue<Proses> realTimeQueue) {
+		this.realTimeQueue = realTimeQueue;
+	}
+	
+	// Prosesin hazýr olup olmadýðýnýn kontrolü
+	public void isReady() {
+		// prosesin geliþ zamanýna göre hazýr durumuna geçmesi
+		for(Proses item:realTimeQueue) {
+			if(item.getVarisZamani()==Time.time) {
+				item.setDurum("hazýr");
+				Proses temp=realTimeQueue.remove();
+				readyQueuePrio0.add(temp);
+			}
+		}
+	}
+	//Önceliði 0 olan kuyruðun çalýþmasý
+	public void go(Proses item) {
+		item.setDurum("çalýþýyor");
+		item.setPatlamaZamani(item.getPatlamaZamani()-1);
+		item.zamanAsimi=21;
+		Dispatcher.isStart=false;
+		if(item.getPatlamaZamani()==0) { //Bittiðnde yapýlacaklar
+			item.setDurum("bitti");
+			readyQueuePrio0.remove(item);
+			Dispatcher.isStart=true;
+		}
+		System.out.println(item);
+	}
+	
+	public Queue<Proses> getReadyQueuePrio0() {
+		return readyQueuePrio0;
+	}
+
+	public void setReadyQueuePrio0(Queue<Proses> readyQueuePrio0) {
+		this.readyQueuePrio0 = readyQueuePrio0;
+	}
+}
